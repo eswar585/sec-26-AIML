6a431d12689fdd8bf8f70b0c2bf96ecd4c4d43e0
eswar585 <eswaravuthu04@gmail.com>
2026-07-07 12:08:17 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,30 +1,51 @@
 function login(){
 
-const regNo =
-document.getElementById("regNo").value.trim();
+    const regNo =
+    document.getElementById("regNo").value.trim();
 
-const password =
-document.getElementById("password").value.trim();
+    const password =
+    document.getElementById("password").value.trim();
 
-const student =
-studentDatabase[regNo];
+    const student =
+    studentDatabase[regNo];
 
-if(student && student.password === password){
+    if(student && student.password === password){
 
-localStorage.setItem(
-"loggedInStudent",
-regNo
-);
+        localStorage.setItem(
+            "loggedInStudent",
+            regNo
+        );
 
-window.location.href =
-"marks.html";
+        window.location.href = "marks.html";
+
+    }
+    else{
+
+        document.getElementById("error").innerText =
+        "Invalid Registration Number or Password";
+
+    }
 
 }
-else{
 
-document.getElementById("error").innerText =
-"Invalid Registration Number or Password";
+function togglePassword(){
 
-}
+    const password =
+    document.getElementById("password");
+
+    if(password.type === "password"){
+
+        password.type = "text";
+
+        document.querySelector(".toggle-password").innerHTML="🙈";
+
+    }
+    else{
+
+        password.type = "password";
+
+        document.querySelector(".toggle-password").innerHTML="👁️";
+
+    }
 
 }
